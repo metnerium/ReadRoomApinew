77720d6ebe9684c048711e34ac3dfd7f66870acf
meit <metnerium@duck.com>
2024-10-17 20:50:41 +0300

    fix login

--- app/models/user.py
+++ app/models/user.py
@@ -10,7 +10,7 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     # email = Column(String, unique=True, index=True)
-    vk_id = Column(Integer, index=True)
+    vk_id = Column(Integer, unique=True, index=True)
     # hashed_password = Column(String, nullable=False)
     full_name = Column(String)
     pseudonym = Column(String, unique=True, nullable=True)
--- app/routes/auth.py
+++ app/routes/auth.py
@@ -6,7 +6,7 @@ from datetime import timedelta
 
 from app.models.user import User
 from app.schemas.user import Token
-from app.utils.security import create_access_token, verify_password, is_valid
+from app.utils.security import create_access_token, verify_password, is_valid, verify_url
 from dependencies import get_db
 from config import ACCESS_TOKEN_EXPIRE_MINUTES
 from app.schemas.user import UserLogin
@@ -14,20 +14,29 @@ router = APIRouter()
 
 @router.post("/token", response_model=Token)
 async def login_for_access_token(
-    user: UserLogin,
+    user_login: UserLogin,
     db: AsyncSession = Depends(get_db)
 ):
-    user = await db.scalar(select(User).filter(User.vk_id == user.vk_id))
-    if not user or not is_valid(user.url):
+    # Сначала проверяем URL
+    if not verify_url(user_login.url):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="User not found or invalid",
+            detail="Invalid URL signature",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    # Затем ищем пользователя
+    user = await db.scalar(select(User).filter(User.vk_id == user_login.vk_id))
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User not found",
             headers={"WWW-Authenticate": "Bearer"},
         )
 
     access_token_expires = timedelta(minutes=int(ACCESS_TOKEN_EXPIRE_MINUTES))
     access_token = create_access_token(
-        data={"sub": user.vk_id}, expires_delta=access_token_expires
+        data={"sub": str(user.vk_id)}, expires_delta=access_token_expires
     )
 
     return {"access_token": access_token, "token_type": "bearer"}
